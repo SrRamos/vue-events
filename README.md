f5b29d7e9ce708f09006b889eb9a3702be3fd43e
Edward S. Ramos <edwardramosp@gmail.com>
2020-07-17 10:31:15 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Vue Events
+
+App for add create and list events.
+
+To install dependencies:
+
+```
+    $ npm install
+```
+
+To run the server:
+
+```
+    $ npm run serve
+```
+
+To get json-server, which we use for our mock database (Starting on API calls with Axios)
+
+```
+    $ npm install -g json-server
+```
+
+To run the mock database
+
+```
+    $ json-server --watch db.json
+```
+
+## TODO:
+ - Change font size for whole page to 16px
+ - Add route to `event/create` with name `event-create`
+ - Add 404 personalized page
+ - Fix errors
+ - Fix styles
+ - Refactor code
+ - Change pagination to 5 per page
+ - Add remove event
+ - When add a event redirect to event detail
---
+++ src/views/EventShow.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="event-header">
+      <span class="eyebrow">@{{ event.time }} on {{ event.date | date }}</span>
+      <span class="title">{{ event.title }}</span>
+      <span>Organized by {{ event.organizer ? event.organizer.name : '' }}</span>
+      <span>Category: {{ event.category }}</span>
+    </div>
+
+    <BaseIcon class="location" name="map">
+      <h2>Location</h2>
+    </BaseIcon>
+
+    <span>{{ event.location }}</span>
+
+    <h2>Event details</h2>
+    <p>{{ event.description }}</p>
+
+    <h2>
+      Attendees
+      <span class="badge -fill-gradient">{{ event.attendees ? event.attendees.length : 0 }}</span>
+    </h2>
+    <ul class="list-group">
+      <li v-for="(attendee, index) in event.attendees" :key="index" class="list-item">
+        <b>{{ attendee.name }}</b>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import { mapActions } from 'vuex'
+export default {
+  props: {
+    event: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+<style module>
+.location {
+  margin-bottom: 0;
+}
+.location > .icon {
+  margin-left: 10px;
+}
+.title {
+  margin: 0;
+}
+.list-group {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+.list-group > .list-item {
+  padding: 1em 0;
+  border-bottom: solid 1px #e5e5e5;
+}
+</style>
